400ddebfe90c4d447c413e08d3e3e613e9da39c9
AjibsBaba <samuelajibade22@gmail.com>
2022-04-02 02:57:35 +0100

    created launcher activity

---
+++ app/src/main/java/com/ajibsbaba/retrofit4dummies/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ajibsbaba.retrofit4dummies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
