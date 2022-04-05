0f6e52649eb22d2ef69d6d3f93c2feb0b6dd3834
AjibsBaba <samuelajibade22@gmail.com>
2022-04-05 11:10:17 +0100

    successfully implemented recycler view

--- app/src/main/java/com/ajibsbaba/retrofit4dummies/MainActivity.kt
+++ app/src/main/java/com/ajibsbaba/retrofit4dummies/MainActivity.kt
@@ -2,26 +2,44 @@ package com.ajibsbaba.retrofit4dummies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ajibsbaba.retrofit4dummies.adapter.MyAdapter
 import com.ajibsbaba.retrofit4dummies.repository.Repository
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private val myAdapter by lazy { MyAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupRecyclerView()
+
         val repository = Repository()
         val viewModelFactory = MainViewModelFactory(repository)
         viewModel =  ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+        viewModel.getCustomPosts(2, "id", "desc")
+        viewModel.myCustomPosts.observe(this, Observer { response ->
+            if (response.isSuccessful){
+                response.body()?.let { myAdapter.setData(it) }
+            } else {
+                Toast.makeText(this, response.code(), Toast.LENGTH_SHORT).show()
+            }
+
+        })
 
 
+    }
+
+
+    private fun setupRecyclerView(){
+        recyclerview.adapter = myAdapter
+        recyclerview.layoutManager = LinearLayoutManager(this)
     }
 }
--- app/src/main/java/com/ajibsbaba/retrofit4dummies/adapter/MyAdapter.kt
+++ app/src/main/java/com/ajibsbaba/retrofit4dummies/adapter/MyAdapter.kt
@@ -1,26 +1,36 @@
 package com.ajibsbaba.retrofit4dummies.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
+import com.ajibsbaba.retrofit4dummies.R
 import com.ajibsbaba.retrofit4dummies.model.Post
+import kotlinx.android.synthetic.main.row_layout.view.*
 
 class MyAdapter: RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
-    private  var myList = emptyList<Post>()
+    private var myList = emptyList<Post>()
 
     inner class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("Not yet implemented")
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.row_layout, parent, false))
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return myList.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.itemView.userId_txt.text = myList[position].userID.toString()
+        holder.itemView.id_txt.text = myList[position].id.toString()
+        holder.itemView.title_txt.text = myList[position].title
+        holder.itemView.body_txt.text = myList[position].body
+    }
+
+    fun setData(newList: List<Post>) {
+        myList = newList
+        notifyDataSetChanged()
     }
 }
